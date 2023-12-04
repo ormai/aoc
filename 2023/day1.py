576c9b7036315bd60f721d8a5d53d751ca47a6b4
Mario D'Andrea <goffredo2004@gmail.com>
2023-12-04 20:46:22 +0100

    Celean up

--- 2023/day1.py
+++ 2023/day1.py
@@ -1,62 +1,45 @@
 import regex
 
-with open('data/day1.txt') as file:
-    sum = 0
-    for line in file:
 
-        value = ''
+def solution():
+    with open("data/day1.txt") as file:
+        DIGIT = r"one|two|three|four|five|six|seven|eight|nine|1|2|3|4|5|6|7|8|9"
+        sum = 0
+        for line in file:
+            value = ""
 
-        number = regex.search(r'one|two|three|four|five|six|seven|eight|nine|1|2|3|4|5|6|7|8|9', line)
-        if number:
-            if not number.group().isdigit():
-                match number.group():
-                    case 'one':
-                        value += '1'
-                    case 'two':
-                        value += '2'
-                    case 'three':
-                        value += '3'
-                    case 'four':
-                        value += '4'
-                    case 'five':
-                        value += '5'
-                    case 'six':
-                        value += '6'
-                    case 'seven':
-                        value += '7'
-                    case 'eight':
-                        value += '8'
-                    case 'nine':
-                        value += '9'
-            else:
-                value += number.group()
+            l = regex.search(DIGIT, line)
+            if l:
+                value += l.group() if l.group().isdigit() else toDigit(l.group())
 
-        number = regex.search(r'one|two|three|four|five|six|seven|eight|nine|1|2|3|4|5|6|7|8|9', line, regex.REVERSE)
-        if number:
-            if not number.group().isdigit():
-                match number.group():
-                    case 'one':
-                        value += '1'
-                    case 'two':
-                        value += '2'
-                    case 'three':
-                        value += '3'
-                    case 'four':
-                        value += '4'
-                    case 'five':
-                        value += '5'
-                    case 'six':
-                        value += '6'
-                    case 'seven':
-                        value += '7'
-                    case 'eight':
-                        value += '8'
-                    case 'nine':
-                        value += '9'
-            else:
-                value += number.group()
+            r = regex.search(DIGIT, line, regex.REVERSE)
+            if r:
+                value += r.group() if r.group().isdigit() else toDigit(r.group())
 
-        print(value)
+            sum += int(value)
+        print(sum)
 
-        sum += int(value)
-    print(sum)
+
+def toDigit(word):
+    match word:
+        case "one":
+            return "1"
+        case "two":
+            return "2"
+        case "three":
+            return "3"
+        case "four":
+            return "4"
+        case "five":
+            return "5"
+        case "six":
+            return "6"
+        case "seven":
+            return "7"
+        case "eight":
+            return "8"
+        case "nine":
+            return "9"
+
+
+solution()
--- 2023/day2.py
+++ 2023/day2.py
@@ -1,23 +1,27 @@
-sum = 0
-with open('data/day2.txt') as file:
-    for line in file:
-        cond = True
-        id = int(line[5:line.index(':')])
-        cubes = line[line.index(':') + 2:].strip(' \n').replace(';', ',').split(', ')
+def solution():
+    sum = 0
+    with open("data/day2.txt") as file:
+        for line in file:
+            cond = True
+            id = int(line[5 : line.index(":")])
+            cubes = (
+                line[line.index(":") + 2 :].strip(" \n").replace(";", ",").split(", ")
+            )
 
-        red = 0
-        green = 0
-        blue = 0
-        for i in range(len(cubes)):
-            count, color = cubes[i].split(' ')
-            if color == 'red':
-                if int(count) > red:
-                    red = int(count);
-            elif color == 'green':
-                if int(count) > green:
-                    green = int(count)
-            else:
-                if int(count) > blue:
-                    blue = int(count)
-        sum += red * green * blue;
-print(sum)
+            red = green = blue = 0
+            for i in range(len(cubes)):
+                count, color = cubes[i].split(" ")
+                if color == "red":
+                    if int(count) > red:
+                        red = int(count)
+                elif color == "green":
+                    if int(count) > green:
+                        green = int(count)
+                else:
+                    if int(count) > blue:
+                        blue = int(count)
+            sum += red * green * blue
+    print(sum)
+
+
+solution()
